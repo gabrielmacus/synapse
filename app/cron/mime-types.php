ab54e1cce681419b76b141d9d3f42680771f8786
Gabriel <gabrielmacus@gmail.com>
2017-10-27 17:14:02 -0300

    actualizaciones y fixes

---
+++ app/cron/mime-types.php
@@ -0,0 +1,24 @@
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Gabriel
+ * Date: 27/10/2017
+ * Time: 12:44 PM
+ */
+$return = array();
+$mimes = file_get_contents('http://svn.apache.org/repos/asf/httpd/httpd/trunk/docs/conf/mime.types'); // make sure that allow_url_fopen is enabled!
+
+preg_match_all('#^([^\s]{2,}?)\s+(.+?)$#ism', $mimes, $matches, PREG_SET_ORDER);
+
+foreach ($matches as $match){
+    $exts =  explode(" ", $match[2]);
+    foreach ($exts as $ext){
+        $return[$ext]=$match[1];
+    }
+}
+
+$data ='<?php $mime_types='.var_export($return,true).'; ?>';
+
+
+FileService::write($data,BASE_PATH."/app/cache/mimes.php",'w');
--- route.php
+++ route.php
@@ -4,116 +4,121 @@ try
 {
     include ("app/autoload.php");
 
-	
-    switch($uri['action'])
+
+    if($uri["module"]=="static")
     {
-        case 'js':
-        case 'embed':
-        case 'css':
-        case 'downloads':
-        case 'hls':
-            $inc = BASE_PATH.str_replace($_ENV['website']['root'],"",parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH));
-            $ext = explode(".",$inc);
-            $ext = end($ext);
+        $inc = BASE_PATH.str_replace($_ENV['website']['root'],"",parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH));
+        $ext = explode(".",$inc);
+        $ext = end($ext);
 
-            switch ($ext)
-            {
-                case 'm3u8':
-
-                    header('Content-Type: application/vnd.apple.mpegurl');
-
-                    break;
-                case 'ts':
-
-                    header('Content-Type: video/MP2T');
-
-                    break;
-                case 'css':
-                    header("Content-type: text/css");
-                    break;
-                case 'js':
-                    header("Content-type: application/javascript");
-
-                    break;
-            }
-            readfile($inc);
+        $contentType ="text/plain";
 
 
-            break;
-        case 'cron':
-		 $inc = BASE_PATH.str_replace($_ENV['website']['root'],"",parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH));
-          
-		  include($inc);
-		
-		break;
+        switch ($ext)
+        {
+            case 'ts':
 
+                $contentType="video/MP2T";
 
+                break;
 
-
-
-        default:
-
-            $uri['action'] =(!empty($uri['action']))?".{$uri['action']}":$uri['action'];
-
-
-            if(empty($uri["module"]))
-            {
-                $uri["module"]="home";
-                $uri["action"]="";
-            }
-
-            $itemType= $uri["module"];
-
-            
-            //Policies
-
-            if(!empty($_ENV["policies"]["{$uri['module']}{$uri['action']}"]) && is_array($_ENV["policies"]["{$uri['module']}{$uri['action']}"]))
-            {
-
-                foreach ($_ENV["policies"]["{$uri['module']}{$uri['action']}"] as $policy)
+            default:
+                if(file_exists(BASE_PATH."/app/cache/mimes.php"))
                 {
+                    include (BASE_PATH."/app/cache/mimes.php");
 
-                    $policiesInc=POLICIES_PATH."/{$policy}.php";
-
-                    if(file_exists($policiesInc))
+                    if(!empty($mime_types[$ext]))
                     {
-                        include ($policiesInc);
+                        $contentType =$mime_types[$ext];
                     }
                 }
+                break;
+        }
 
+        header("Content-Type: {$contentType}");
+        readfile($inc);
 
-            }    
-
-            //Validation
-            $validationInc = BASE_PATH."/app/validations/{$uri['module']}{$uri['action']}.php";
-            if( !empty($_GET["act"]) && file_exists($validationInc))
-            {
-                include ($validationInc);
-            }
-            
-
-
-
-            include (CONTROLLER_PATH."/{$uri['module']}{$uri['action']}.php");
-
-            if(!empty($incBody))
-            {
-                if(empty($incLayout))
-                {
-                    $incLayout = TEMPLATE_PATH."/layouts/layout.php";
-                }
-
-                if(empty($incNavbar))
-                {
-                    $incNavbar = TEMPLATE_PATH."/navbars/navbar.php";
-                }
-                include $incLayout;
-
-            }
-
-        break;
 
     }
+    else
+    {
+        switch($uri['action'])
+        {
+
+            case 'cron':
+                $inc = BASE_PATH.str_replace($_ENV['website']['root'],"",parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH));
+
+                include($inc);
+
+                break;
+
+            default:
+
+                $uri['action'] =(!empty($uri['action']))?".{$uri['action']}":$uri['action'];
+
+
+                if(empty($uri["module"]))
+                {
+                    $uri["module"]="home";
+                    $uri["action"]="";
+                }
+
+                $itemType= $uri["module"];
+
+
+                //Policies
+
+                if(!empty($_ENV["policies"]["{$uri['module']}{$uri['action']}"]) && is_array($_ENV["policies"]["{$uri['module']}{$uri['action']}"]))
+                {
+
+                    foreach ($_ENV["policies"]["{$uri['module']}{$uri['action']}"] as $policy)
+                    {
+
+                        $policiesInc=POLICIES_PATH."/{$policy}.php";
+
+                        if(file_exists($policiesInc))
+                        {
+                            include ($policiesInc);
+                        }
+                    }
+
+
+                }
+
+                //Validation
+                $validationInc = BASE_PATH."/app/validations/{$uri['module']}{$uri['action']}.php";
+                if( !empty($_GET["act"]) && file_exists($validationInc))
+                {
+                    include ($validationInc);
+                }
+
+
+
+
+                include (CONTROLLER_PATH."/{$uri['module']}{$uri['action']}.php");
+
+                if(!empty($incBody))
+                {
+                    if(empty($incLayout))
+                    {
+                        $incLayout = TEMPLATE_PATH."/layouts/layout.php";
+                    }
+
+                    if(empty($incNavbar))
+                    {
+                        $incNavbar = TEMPLATE_PATH."/navbars/navbar.php";
+                    }
+                    include $incLayout;
+
+                }
+
+                break;
+
+        }
+
+    }
+
+
 
 }
 catch(Exception $e)
